b9d7728fe365781d5ed115386f46e0ef5b9c7e76
Raven Cunanan <63638637+RavenCunanan@users.noreply.github.com>
2025-05-19 11:36:43 -0700

    Update player.py

--- code/player.py
+++ code/player.py
@@ -3,5 +3,5 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load(join('images', 'player', 'down', '0.png')).convert_alpha
-        self.rect = self.image.get_frect(center = pos)
+        self.image = pygame.image.load(join('images', 'player', 'down', '0.png')).convert_alpha()
+        self.rect = self.image.get_frect(center=pos)
