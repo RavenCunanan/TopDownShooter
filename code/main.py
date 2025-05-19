c9e3ffb3369d7ac3077fa7a1d4553511ab163c01
Raven Cunanan <63638637+RavenCunanan@users.noreply.github.com>
2025-05-19 11:36:26 -0700

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,12 +1,20 @@
 from settings import *
+from player import Player
 
 class Game:
     def __init__(self):
+        # setup
         pygame.init()
-        self.display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Top Down Shooter')
         self.clock = pygame.time.Clock()
         self.running = True
+
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+
+        # sprites
+        self.player = Player((400,300), self.all_sprites)
     
     def run(self):
         while self.running:
@@ -19,8 +27,9 @@ class Game:
                     self.running = False
             
             # update
-
+            self.all_sprites.update(dt)
             # draw
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
 
         pygame.quit()
