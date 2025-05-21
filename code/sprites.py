61ed98894f004c04dd21a41f210278970aa02edf
Raven Cunanan <63638637+RavenCunanan@users.noreply.github.com>
2025-05-20 17:41:35 -0700

    Update sprites.py

--- code/sprites.py
+++ code/sprites.py
@@ -4,6 +4,6 @@ class CollisionSprite(pygame.sprite.Sprite):
     def __init__(self, pos, size, groups):
         super().__init__(groups)
         self.image = pygame.Surface((size))
-        self.rect.fill('blue')
+        self.image.fill('blue')
         self.rect = self.image.get_rect(center=pos)
         
