516cd2ccecbfb23f61438aa314530689f0ce351e
Raven Cunanan <63638637+RavenCunanan@users.noreply.github.com>
2025-06-10 17:39:49 -0700

    Update sprites.py

--- code/sprites.py
+++ code/sprites.py
@@ -1,4 +1,5 @@
 from settings import *
+from math import atan2, degrees
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups):
@@ -19,7 +20,7 @@ class Gun(pygame.sprite.Sprite):
         # player connection
         self.player = player
         self.distance = 140
-        self.player_direction = pygame.Vector2(1, 0)
+        self.player_direction = pygame.Vector2(0,1)
 
         #sprite setup
         super().__init__(groups)
@@ -27,4 +28,22 @@ class Gun(pygame.sprite.Sprite):
         self.image = self.gun_surf
         self.rect = self.image.get_frect(center = self.player.rect.center + self.player_direction * self.distance)
 
-              
+    def get_direction(self):
+        mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
+        player_pos = pygame.Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+        self.player_direction= (mouse_pos - player_pos).normalize()
+        print(self.player_direction)
+
+    def rotate_gun(self):
+        angle = degrees(atan2(self.player_direction.x, self.player_direction.y)) - 90
+        if self.player_direction.x > 0:
+            self.image = pygame.transform.rotozoom(self.gun_surf, angle, 1)
+        else:
+            self.image = pygame.transform.rotozoom(self.gun_surf, abs(angle), 1)
+            self.image =pygame.transform.flip(self.image, False, True)
+    
+    def update(self, _):
+        self.get_direction()
+        self.rotate_gun()
+        self.rect.center = self.player.rect.center + self.player_direction * self.distance
+
